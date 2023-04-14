400b75daba87fc1dc40be49017e43d103ed8c0f0
jeremycolfer27 <ii21038@bristol.ac.uk>
2023-04-14 17:35:57 +0100

    fixed desktop cam access

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const flipButton = document.getElementById('flipButton');
 
 let isFlipped = false;
 
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-// Request access to the user's camera
-navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } } })
+if (isMobile) {
+  console.log('The user is on a mobile device: ', navigator.userAgent);
+  // Request access to the user's camera
+  navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } } })
   .then((stream) => {
     // Set the video element's source object to the camera stream
     video.srcObject = stream;
@@ -13,8 +16,25 @@ navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment
   .catch((error) => {
     console.error('Could not access camera:', error);
   });
+} 
 
-  flipButton.addEventListener('click', function(){
+
+else {
+    console.log('The user is on a desktop device: ', navigator.userAgent);
+    // Request access to the desktop camera
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then((stream) => {
+      // Set the video element's source object to the camera stream
+      video.srcObject = stream;
+    })
+    .catch((error) => {
+      console.error('Could not access camera:', error);
+    });
+}
+
+
+
+flipButton.addEventListener('click', function(){
     if(isFlipped){
         video.style.transform = "scaleX(1)";
         isFlipped = false;
